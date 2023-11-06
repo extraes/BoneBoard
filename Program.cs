cd746d17ffc349dd3ff9bba7fb318b4b8d46932e
extraes <52384576+extraes@users.noreply.github.com>
2023-11-06 14:38:56 -0800

    pre-warm persistentdata so that if there are any errors, they get caught before attempting to log in to discord

--- Program.cs
+++ Program.cs
@@ -6,6 +6,9 @@
 
         static async Task Main(string[] args)
         {
+            Logger.Put("Pre-initializing persistent data...");
+            _ = PersistentData.values;
+
             Logger.Put("Initializing bot...");
             bot = new BoneBot(Config.values.token);
             bot.Init();
