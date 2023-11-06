59f79154a9c033b8c5c6c034cb3104ee446f9d55
extraes <52384576+extraes@users.noreply.github.com>
2023-11-06 14:42:03 -0800

    Add new cmds & fix strings
    Also rename perms constant because that was stolen from voidway and this doesnt do the same things lol

--- SlashCommands.cs
+++ SlashCommands.cs
@@ -12,14 +12,14 @@ namespace BoneBoard;
 
 internal class SlashCommands : ApplicationCommandModule
 {
-    const Permissions FORCE_QUOTE_PERMS = Permissions.ManageRoles | Permissions.ManageMessages;
+    const Permissions GUARDED_CMD_PERMS = Permissions.ManageRoles | Permissions.ManageMessages;
     
     [SlashCommand("createFrogMsg", "Creates the message that must be reacted to for KOTF (King of the Frog)")]
-    [SlashRequireUserPermissions(FORCE_QUOTE_PERMS, false)]
+    [SlashRequireUserPermissions(GUARDED_CMD_PERMS, false)]
     [SlashRequireBotPermissions(Permissions.AddReactions | Permissions.ManageMessages, false)]
     public async Task CreateFrogMessage(
         InteractionContext ctx,
-        [Option("msgContent", "The text content of the message. Use {0} to indicate where the leaderboard goes.")]
+        [Option("msgContent", "The text content of the message. Use {0} to indicate where the current king goes.")]
         string content
         )
     {
@@ -29,7 +29,7 @@ internal class SlashCommands : ApplicationCommandModule
             return;
         }
 
-        if (!ctx.Member.Permissions.HasPermission(FORCE_QUOTE_PERMS))
+        if (!ctx.Member.Permissions.HasPermission(GUARDED_CMD_PERMS))
         {
             await ctx.CreateResponseAsync("nuh uh", true);
             return;
@@ -48,12 +48,39 @@ internal class SlashCommands : ApplicationCommandModule
         await ctx.CreateResponseAsync("Created KOTF message!", true);
     }
 
+    [SlashCommand("setFrogUnavailableText", "Set the text that will be displayed when someone tries to get the frog role when it's not available.")]
+    [SlashRequireUserPermissions(GUARDED_CMD_PERMS, false)]
+    [SlashRequireBotPermissions(Permissions.AddReactions | Permissions.ManageMessages, false)]
+    public async Task SetFrogUnavailableText(
+        InteractionContext ctx,
+        [Option("msgContent", "The text content of the message. Use {0} to for curr king, and {1} available day of week.")]
+        string content
+        )
+    {
+        if (ctx.Member is null)
+        {
+            await ctx.CreateResponseAsync("😂👎", true);
+            return;
+        }
+
+        if (!ctx.Member.Permissions.HasPermission(GUARDED_CMD_PERMS))
+        {
+            await ctx.CreateResponseAsync("nuh uh", true);
+            return;
+        }
+
+        Config.values.frogMessageClosedBase = content;
+        Config.WriteConfig();
+
+        await ctx.CreateResponseAsync("Set frog unavailable text!", true);
+    }
+
     [SlashCommand("createFrogLeaderboardMsg", "Creates the message that must be reacted to for KOTF (King of the Frog)")]
-    [SlashRequireUserPermissions(FORCE_QUOTE_PERMS, false)]
+    [SlashRequireUserPermissions(GUARDED_CMD_PERMS, false)]
     [SlashRequireBotPermissions(Permissions.AddReactions | Permissions.ManageMessages, false)]
     public async Task CreateFrogLeaderboardMsg(
-       InteractionContext ctx,
-       [Option("msgContent", "The text content of the message. Use {0} to indicate where the leaderboard goes (after a multiline).")]
+        InteractionContext ctx,
+        [Option("msgContent", "Use {0} to indicate where the leaderboard goes (Will be surrounded by multilines)")]
         string content
        )
     {
@@ -63,21 +90,82 @@ internal class SlashCommands : ApplicationCommandModule
             return;
         }
 
-        if (!ctx.Member.Permissions.HasPermission(FORCE_QUOTE_PERMS))
+        if (!ctx.Member.Permissions.HasPermission(GUARDED_CMD_PERMS))
         {
             await ctx.CreateResponseAsync("nuh uh", true);
             return;
         }
 
         DiscordMessage msg = await ctx.Channel.SendMessageAsync(content.Replace("{0}", "\n(There'll be a leaderboard here...)\n"));
-        //await msg.CreateReactionAsync(DiscordEmoji.FromUnicode("🐸"));
 
         Config.values.frogLeaderboardLink = msg.JumpLink.OriginalString;
         Config.values.frogLeaderboardBase = content;
         Config.WriteConfig();
         await BoneBot.Bots[ctx.Client].FetchFrogLeaderboardMsg();
 
-        await ctx.CreateResponseAsync("Created KOTF message!", true);
+        await ctx.CreateResponseAsync("Created KOTF leaderboard message! You may need to delete the old one!", true);
+    }
+
+
+    [SlashCommand("removeUserFromFrogLeaderboard", "Remove user from frog role leaderboard. De-role/block/ban them before use.")]
+    [SlashRequireUserPermissions(GUARDED_CMD_PERMS, false)]
+    [SlashRequireBotPermissions(Permissions.AddReactions | Permissions.ManageMessages, false)]
+    public async Task RemoveUserFromLeaderboard(
+        InteractionContext ctx,
+        [Option("user", "Whose ID to remove from the leaderboard. Won't trigger leaderboard refresh.")]
+        DiscordUser? user = null,
+        [Option("userId","The ID to remove from the leaderboard. Won't trigger leaderboard refresh.")]
+        long? userId = null
+       )
+    {
+        if (ctx.Member is null)
+        {
+            await ctx.CreateResponseAsync("😂👎", true);
+            return;
+        }
+
+        if (!ctx.Member.Permissions.HasPermission(GUARDED_CMD_PERMS))
+        {
+            await ctx.CreateResponseAsync("nuh uh", true);
+            return;
+        }
+
+        ulong? id = (ulong?)userId ?? user?.Id;
+        if (!id.HasValue)
+        {
+            await ctx.CreateResponseAsync("care to elaborate on whos gettin the ax?", true);
+            return;
+        }
+
+        bool wasInData = PersistentData.values.frogRoleTimes.Remove(id.Value);
+        PersistentData.WritePersistentData();
+
+        await ctx.CreateResponseAsync(wasInData ? "Removed that user from the frog leaderboard." : "That user wasn't in the leaderboard (yet?).", true);
+    }
+
+
+    [SlashCommand("reloadCfg", "Reloads the config. This may not have any impact on things that are cached at startup.")]
+    [SlashRequireUserPermissions(GUARDED_CMD_PERMS, false)]
+    [SlashRequireBotPermissions(Permissions.AddReactions | Permissions.ManageMessages, false)]
+    public async Task ReloadConfig(
+        InteractionContext ctx
+       )
+    {
+        if (ctx.Member is null)
+        {
+            await ctx.CreateResponseAsync("😂👎", true);
+            return;
+        }
+
+        if (!ctx.Member.Permissions.HasPermission(GUARDED_CMD_PERMS))
+        {
+            await ctx.CreateResponseAsync("nuh uh", true);
+            return;
+        }
+
+        Config.ReadConfig();
+
+        await ctx.CreateResponseAsync("Read config!", true);
     }
 
     //public async Task SetFrogThread(
